9290e162f628fd5b818dc8a2c3a3164de20a87a7
Gilberto Silva <contatogilbertolopes@outlook.com.br>
2017-11-08 20:42:46 -0200

    Adição de espaço para menu lateral

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	$("#btn_open_menu_lateral").click(function(){
+		$("#menu_esquerda").animate({width:'toggle'},250);
+	});
 
-	
+	$("#menu_esquerda").mouseleave(function(){
+		$("#menu_esquerda").animate({width:'toggle'},250);
+	});	
 });
